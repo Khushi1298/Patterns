f352e6d1d0ddf8466cf71e6322bd28b8872bc290
Khushi singhal <134137454+Khushi1298@users.noreply.github.com>
2023-10-10 19:58:06 +0530

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std ;
+int main()
+{
+   int i , j ;
+   for(i=1;i<=3; i++)
+   {
+     for(j=3;j>=1;j--)
+     {
+        cout<< j ;
+     }
+     cout<< endl ;
+   }
+return 0 ;
+}
