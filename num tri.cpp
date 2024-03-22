c8c41337c57e775029d87e04efc4d7cc26034422
khushi singhal <khushisinghal201120@gmail.com>
2024-03-22 14:23:45 +0530

    $ git pull $ git pull $ git pull i Patterns

---
+++ num tri.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std ;
+int main()
+{
+    int i, j ;
+    int n ;
+    cout <<"Enter n : " ;
+    cin >> n ;
+    for(i=1;i<=n;i++)
+    {
+      for(j=1;j<=i;j++)
+        cout << j ;
+            cout << endl ;
+    }
+}
